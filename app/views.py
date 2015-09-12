fa09496ff0a2ec90dbf3b7f8285a43cc798eeec2
Nate Luzod <nate.luzod@mac.com>
2015-09-12 10:55:55 -1000

    Chapter 3: forms, validation

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
@@ -27,9 +27,9 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
+      flash('Login requested for OpenID="%s", remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
+      return redirect('/index')
     return render_template('login.html', 
-                           title='Sign In',
-                           form=form)
+                            title='Sign In', 
+                            form=form,
+                            providers=app.config['OPENID_PROVIDERS'])
