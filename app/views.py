6c5a6ac16777d56b2d158276d8ce52082e450b26
Nate Luzod <nate.luzod@mac.com>
2015-09-12 02:08:53 -1000

    Routes, templates, blah

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-  return "Hello, World!"
+  user = {'nickname': 'Nate' }
+  title = "Test"
+  posts = [
+    {
+      'author' : {'nickname' : 'John'},
+      'body' : 'Beautiful day in Portland!'
+    },
+    {
+      'author' : {'nickname' : 'Susan'},
+      'body' : 'This is another post you should not read'
+    }
+  ]
+
+
+  return render_template('index.html', title=title, user=user, posts=posts)
