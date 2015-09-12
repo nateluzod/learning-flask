e3f6f24f38b4b3a1d961688360682bd6ee27df55
Nate Luzod <nate.luzod@mac.com>
2015-09-12 02:22:57 -1000

    User logins (step 3) - WIP

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -20,3 +21,15 @@ def index():
 
 
   return render_template('index.html', title=title, user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
